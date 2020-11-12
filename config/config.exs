2837e512679b951f0d26bc49ff54e50bb650758f
Daniel Santiago <danielsantiago111@hotmail.com>
2020-11-12 11:41:15 -0300

    Refactor config to use Elixir config instead of Mix

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,6 @@
-use Mix.Config
+import Config
 
 config :todo, :database, pool_size: 3, folder: "./persist"
 config :todo, port: 5454
 
-import_config "#{Mix.env()}.exs"
+import_config "#{config_env()}.exs"
